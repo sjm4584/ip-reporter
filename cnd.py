bd90a46ff376e81fd31ea8086c367af3f9c08fa0
sjm4584 <sjm4584@rit.edu>
2015-09-17 17:41:57 -0400

    added IP address checking. started ssh monitoring, cleaned up main

--- cnd.py
+++ cnd.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python
 
 import subprocess
+import socket
+import os
 import sys
-print sys.argv[1]
-command = 'whois %s' % (sys.argv[1])
-process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-output = process.communicate()
 
-#Pull the abuse email out so we can bitch
-for item in output[0].split('\n'):
-    if 'OrgAbuseEmail' in item:
-        abuse_email = item.strip().split()
-print abuse_email[1]
+# Lookup the abuse reporting email for that IP using whois
+def whois_lookup(ip):
+    command = 'whois %s' % ip
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    cmd_output = process.communicate()
+
+    for item in cmd_output[0].split('\n'):
+        if 'OrgAbuseEmail' in item:
+            abuse_email = item.strip().split()
+    return abuse_email[1]
+
+
+# Monitor SSH for brute force attempts, pass offenders to whois_lookup()
+def ssh_monitor():
+    if os.path.isfile("/var/log/auth.log"):
+        authlog_f = file("/var/log/auth.log", "r")
+    
+                
+def ip_check(ip):            
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+
+
+def main():
+    if ip_check(sys.argv[1]) == False:
+        print "[+] Error: Bad IP address, exiting..."
+        exit()
+    print '[+] Sending to whois'
+    email = whois_lookup(sys.argv[1])
+    print '[+] Abuse email is: ' + email
+
+
+if __name__ == '__main__':
+    main()
